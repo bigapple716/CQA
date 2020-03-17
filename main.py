53effada8b1e8276862cda8066dc40fba14db822
mike <congcong0716@sina.com>
2020-03-17 19:12:41 -0400

    把clean_answers()独立出来

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 
 from search_algorithms import *
 from reader import Reader
+from post_processor import PostProcessor
 
 # 载入参数
 parser = argparse.ArgumentParser()
@@ -67,50 +68,6 @@ def search_answers(cleaned_in, uncut_in, cleaned_ans_json, cleaned_ans_txt):
     return answers_list, answers_index_list, sorted_scores_list
 
 
-def clean_answers(ans_in, ans_idx_in, cleaned_ans_txt):
-    # 入参检查
-    if len(ans_in) != len(ans_idx_in):
-        raise Exception('答案列表和答案索引列表长度不一致！')
-
-    # 载入答案文档
-    with open(cleaned_ans_txt, 'r') as f_ans_txt:
-        text = f_ans_txt.readlines()
-
-    ret = []
-    for answers, answer_indexes in zip(ans_in, ans_idx_in):
-        cleaned_ans = []
-        for ans, ans_idx in zip(answers, answer_indexes):
-            if re.match(r'\d+\.\d+', ans) is not None:
-                # 如果答案是二级标题，那么就取下一个段落(下个段落有可能是三级标题)
-                next_ans = text[ans_idx + 1].rstrip()
-                next_idx = ans_idx + 1
-                # print('标题：' + ans + ' -> 内容：' + next_ans)
-                # 再判断一下是不是三级标题
-                if re.match(r'\d+\.\d+\.\d+', next_ans) is not None:
-                    real_ans = text[next_idx + 1].rstrip()
-                    cleaned_ans.append(real_ans)
-                    # print('标题：' + next_ans + ' -> 内容：' + real_ans)
-                else:
-                    cleaned_ans.append(next_ans)
-
-            elif re.match(r'\d+\.\d+\.\d+', ans) is not None:
-                # 如果答案是三级标题，那么下一个段落就是真正的答案
-                real_ans = text[ans_idx + 1].rstrip()
-                cleaned_ans.append(real_ans)
-                # print('标题：' + ans + ' -> 内容：' + real_ans)
-
-            elif re.match('.+([？?])$', ans) is not None:
-                # 如果答案以问号结尾，那么就跳过
-                continue
-
-            else:
-                # 如果以上条件都不符合，那么这个答案正常保留
-                cleaned_ans.append(ans)
-
-        ret.append(cleaned_ans)
-    return ret
-
-
 def print_answers(ans_list, output, print2file=True, n_result=3):
     """
         Parameters
@@ -152,6 +109,8 @@ if __name__ == '__main__':
                     cleaned_answers_txt, cleaned_answers_json,
                     long_answers_txt, long_answers_json,
                     cleaned_extra_txt, cleaned_extra_json)  # 实例化一个Reader类
+    post_processor = PostProcessor()
+
     # queries
     with open(input_txt, 'r') as f_input:
         input = f_input.readlines()
@@ -176,7 +135,7 @@ if __name__ == '__main__':
         answers_list, answer_idx_list, sorted_scores_list = \
             search_answers(cleaned_input, uncut_input, long_answers_json, long_answers_txt)
         if method != 'qq-match' and method != 'mix':
-            answers_list = clean_answers(list(answers_list), list(answer_idx_list), long_answers_txt)  # 清洗答案
+            answers_list = post_processor.clean_answers(list(answers_list), list(answer_idx_list), long_answers_txt)  # 清洗答案
         else:
             print_answers(sorted_scores_list, 'data/scores.csv')
     else:
@@ -184,7 +143,7 @@ if __name__ == '__main__':
         answers_list, answer_idx_list, sorted_scores_list = \
             search_answers(cleaned_input, uncut_input, cleaned_answers_json, cleaned_answers_txt)
         if method != 'qq-match' and method != 'mix':
-            answers_list = clean_answers(list(answers_list), list(answer_idx_list), cleaned_answers_txt)  # 清洗答案
+            answers_list = post_processor.clean_answers(list(answers_list), list(answer_idx_list), cleaned_answers_txt)  # 清洗答案
         else:
             print_answers(sorted_scores_list, 'data/scores.csv')
 
---
+++ post_processor.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+
+class PostProcessor:
+    def clean_answers(self, ans_in, ans_idx_in, cleaned_ans_txt):
+        # 入参检查
+        if len(ans_in) != len(ans_idx_in):
+            raise Exception('答案列表和答案索引列表长度不一致！')
+
+        # 载入答案文档
+        with open(cleaned_ans_txt, 'r') as f_ans_txt:
+            text = f_ans_txt.readlines()
+
+        ret = []
+        for answers, answer_indexes in zip(ans_in, ans_idx_in):
+            cleaned_ans = []
+            for ans, ans_idx in zip(answers, answer_indexes):
+                if re.match(r'\d+\.\d+', ans) is not None:
+                    # 如果答案是二级标题，那么就取下一个段落(下个段落有可能是三级标题)
+                    next_ans = text[ans_idx + 1].rstrip()
+                    next_idx = ans_idx + 1
+                    # print('标题：' + ans + ' -> 内容：' + next_ans)
+                    # 再判断一下是不是三级标题
+                    if re.match(r'\d+\.\d+\.\d+', next_ans) is not None:
+                        real_ans = text[next_idx + 1].rstrip()
+                        cleaned_ans.append(real_ans)
+                        # print('标题：' + next_ans + ' -> 内容：' + real_ans)
+                    else:
+                        cleaned_ans.append(next_ans)
+
+                elif re.match(r'\d+\.\d+\.\d+', ans) is not None:
+                    # 如果答案是三级标题，那么下一个段落就是真正的答案
+                    real_ans = text[ans_idx + 1].rstrip()
+                    cleaned_ans.append(real_ans)
+                    # print('标题：' + ans + ' -> 内容：' + real_ans)
+
+                elif re.match('.+([？?])$', ans) is not None:
+                    # 如果答案以问号结尾，那么就跳过
+                    continue
+
+                else:
+                    # 如果以上条件都不符合，那么这个答案正常保留
+                    cleaned_ans.append(ans)
+
+            ret.append(cleaned_ans)
+        return ret
