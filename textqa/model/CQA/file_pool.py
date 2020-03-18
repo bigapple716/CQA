144d3fee83ac38781007600b57ebb5e0bb01c538
mike <congcong0716@sina.com>
2020-03-18 11:26:25 -0400

    加注释

--- textqa/model/CQA/file_pool.py
+++ textqa/model/CQA/file_pool.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 
 import os
+import platform
 
 
 # 文件名集合
 class FilePool:
-    # relative_path = 'textqa/model/CQA/'
-    relative_path = ''
+    if platform.system() == 'Darwin' or 'Windows':
+        relative_path = ''  # 在本地跑的时候用这个
+    else:
+        relative_path = 'textqa/model/CQA/'  # 在正式环境(Linux)跑用这个
+
     project_dir = os.path.join(os.getcwd(), relative_path)
 
-    raw_docx1 = os.path.join(project_dir, 'data/长沙机场知识库(没目录图片表格).docx')
-    raw_docx2 = os.path.join(project_dir, 'data/96566机场问询资料.docx')
-    answers_txt = os.path.join(project_dir, 'data/answers.txt')
-    extra_txt = os.path.join(project_dir, 'data/extra_answers.txt')
-    cleaned_answers_json = os.path.join(project_dir, 'data/cleaned_answers.json')
-    cleaned_answers_txt = os.path.join(project_dir, 'data/cleaned_answers.txt')
-    cleaned_extra_txt = os.path.join(project_dir, 'data/cleaned_extra.txt')
-    cleaned_extra_json = os.path.join(project_dir, 'data/cleaned_extra.json')
-    long_answers_txt = os.path.join(project_dir, 'data/long_answers.txt')
-    long_answers_json = os.path.join(project_dir, 'data/long_answers.json')
-    input_txt = os.path.join(project_dir, 'data/input.txt')
-    output_csv = os.path.join(project_dir, 'data/output.csv')
-    stopword_txt = os.path.join(project_dir, 'data/stopword.txt')
+    # 原始文档
+    raw_docx1 = os.path.join(project_dir, 'data/长沙机场知识库(没目录图片表格).docx')  # 长沙机场知识文档
+    raw_docx2 = os.path.join(project_dir, 'data/96566机场问询资料.docx')  # 昆明机场知识文档
+    answers_txt = os.path.join(project_dir, 'data/answers.txt')  # 全部机场知识文档的txt格式集合(不含补充知识文档)
+    extra_txt = os.path.join(project_dir, 'data/extra_answers.txt')  # 补充知识文档
+
+    # 清洗过的答案库
+    cleaned_answers_txt = os.path.join(project_dir, 'data/cleaned_answers.txt')  # 短答案(没分词)
+    cleaned_answers_json = os.path.join(project_dir, 'data/cleaned_answers.json')  # 短答案(已分词)
+    cleaned_extra_txt = os.path.join(project_dir, 'data/cleaned_extra.txt')  # 补充答案(没分词)
+    cleaned_extra_json = os.path.join(project_dir, 'data/cleaned_extra.json')  # 补充答案(已分词)
+    long_answers_txt = os.path.join(project_dir, 'data/long_answers.txt')  # 长答案(没分词)
+    long_answers_json = os.path.join(project_dir, 'data/long_answers.json')  # 长答案(已分词)
+
+    # 输入输出
+    input_txt = os.path.join(project_dir, 'data/input.txt')  # 问题queries，仅在批量测试时用
+    output_csv = os.path.join(project_dir, 'data/output.csv')  # 输出的回答，仅在批量测试时用
+
+    stopword_txt = os.path.join(project_dir, 'data/stopword.txt')  # 停用词表
 
     docx_list = [raw_docx1, raw_docx2]
