60776a553ef5cd7e7f23fe754bf06a78fb89d3f4
mike <congcong0716@sina.com>
2020-03-22 13:12:26 -0400

    简化代码

--- textqa/model/CQA/utils.py
+++ textqa/model/CQA/utils.py
@@ -10,35 +10,6 @@ from textqa.model.CQA.file_pool import FilePool
 
 # 静态工具类
 class Utils:
-    @staticmethod
-    def trim_result(score_list, pos_list, threshold, bigger_is_better=True):
-        """
-        得分小于/大于threshold的回答直接丢弃
-
-        Parameters
-        ----------
-        score_list
-        pos_list
-        threshold
-        bigger_is_better : boolean
-            得分越高越好/丢弃掉得分过低的回答；默认为True
-
-        Returns
-        -------
-
-        """
-        if bigger_is_better:
-            # 得分 小于 threshold的回答要被丢弃
-            for i in range(len(score_list)):
-                if score_list[i] < threshold:
-                    pos_list[i] = -1
-        else:
-            # 得分 大于 threshold的回答要被丢弃
-            for i in range(len(score_list)):
-                if score_list[i] > threshold:
-                    pos_list[i] = -1
-        return pos_list
-
     # 判断一个句子是不是标题
     @staticmethod
     def is_heading(sentence):
