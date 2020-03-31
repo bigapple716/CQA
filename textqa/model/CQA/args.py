2b457c4ce8f4d841081037cce9322fefe4e0c0ce
mike <congcong0716@sina.com>
2020-03-31 00:26:53 -0400

    调整QA匹配阈值

--- textqa/model/CQA/args.py
+++ textqa/model/CQA/args.py
@@ -7,11 +7,10 @@ from textqa.model.CQA.method import Method
 method = Method.mix  # 检索方法
 trim_stop = True  # 是否去停用词
 answer_base = 'small'  # 答案库来源: long, cleaned, small
-categorize_question = False  # 是否对问题进行分类
+categorize_question = True  # 是否对问题进行分类
 top_n = 3  # 返回前top_n个结果
 qq_threshold = 0.7  # QQ匹配的阈值
-# qa_threshold = 5  # QA匹配的阈值
-qa_threshold = 1  # QA匹配的阈值
+qa_threshold = 5  # QA匹配的阈值
 syn_threshold = 0.7  # 近义词匹配的阈值
 
 enable_log = False  # 是否向控制台打印log
