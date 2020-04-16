c4d6063d43d8ddcb6cab3e2631fd86248eb8dd06
mike <congcong0716@sina.com>
2020-04-16 00:37:55 -0400

    新增用全集答案库计算IDF

--- textqa/model/CQA/args.py
+++ textqa/model/CQA/args.py
@@ -7,6 +7,7 @@ method = Method.mix  # 检索方法
 trim_stop = True  # 是否去停用词
 answer_base = 'small'  # 答案库来源: long, cleaned, small
 categorize_question = True  # 是否对问题进行分类
+uni_idf = False  # 是否用全集答案库计算IDF
 top_n = 3  # 返回前top_n个结果
 qq_threshold = 0.7  # QQ匹配的阈值
 qa_threshold = 1.5  # QA匹配的阈值
